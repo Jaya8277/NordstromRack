8075a23fb812ef373946a7ae53fd7588709bc19b
Jaya8277 <jayavish423@gmail.com>
2022-05-09 15:54:23 +0530

    Complete Project

---
+++ src/Components/Gift/Gift.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Momgift, { Motherfaq, Therealdeal } from "./Momgift";
+import Shopyourway from "./Shopyourway";
+
+const Gift = () => {
+  return (
+    <div>
+      <Momgift />
+      <Therealdeal />
+      <Motherfaq />
+      <Shopyourway />
+    </div>
+  );
+};
+
+export default Gift;
